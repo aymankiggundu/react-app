ab677caaffb9c8aa3d40a600fe01248f92027040
Kiggundu Sulaiman <kiggundusulaiman332@gmail.com>
2025-06-18 02:14:53 +0300

    first one

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Message from "./Message";
+
+
+function App() {
+  return <div><Message/></div>;
+  //JSX Javascript XML
+  
+}
+export default App;
+ 
---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+function Message(){
+    const name = "ayman";
+    if (name)
+        return <h1>hello  {name} kiggundu</h1>;  //JSX Javascript XML
+    return <h1>hello stranger</h1>;
+}
+export default Message;
